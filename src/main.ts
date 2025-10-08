8661633275f82639f7a6a0a5395703755dacfcbf
benho612 <151803987+benho612@users.noreply.github.com>
2025-10-08 00:48:09 +0000

    Added a button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+/*import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+`;*/
+
+let counter: number = 0;
+
+// Create basic HTML structure
+document.body.innerHTML = `
+  <h1>CMPM 121 Project</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+  console.log("I have these thingies:", button, counterElement, counter);
+});
